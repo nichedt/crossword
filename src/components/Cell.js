3a27196ebe89c32d60b219b9235aaf5499d214b3
Nicholas Tan <nicholas_tan@apple.com>
2022-09-12 08:52:23 -0700

    better grid with hooks

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
-const Cell = ({ cell }) => {
-    return (
-        <div className='cell'><div className='box'>
-            <div className='marker'>{cell.marker}</div>
-            <div className='letter'>{cell.letter}</div>
-        </div></div>
-    )
+const Cell = ({ cell, onCursor }) => {
+  return (
+    <div className='cell' onClick={() => onCursor(cell.i, cell.j)}>
+      <div className={
+        `box ${ cell.isCursor ? 'cursor' : '' } ${ cell.isBlank ? 'blank' : '' }`
+      }>
+        <div className='marker'>{cell.marker}</div>
+        <div className='letter'>{cell.letter}</div>
+      </div>
+    </div>
+  )
 }
 
 export default Cell
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Cell from './Cell'
 
-const Grid = ({ cells }) => {
+const Grid = ({ cells, onCursor }) => {
   return ( <div className='grid'> {
     cells.map((row, i) => { return (
       <div className='row' key={i}> {row.map((cell, j) => { return (
-        <Cell key={j} cell={cell} />
+        <Cell key={j} cell={cell} onCursor={onCursor}/>
       ) })} </div>
     ) }
   )} </div> )
